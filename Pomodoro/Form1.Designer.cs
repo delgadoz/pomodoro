e53491fd9380e20db2aea22a54e0226b5728ffd0
Rodrigo Delgado <rodrigo21delgado@gmail.com>
2022-04-02 00:37:44 -0300

    Primeira versão

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTempo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.timerEstudo = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelProxDescanso = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelCiclos = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timerDescanso = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTempo
+            // 
+            this.labelTempo.AutoSize = true;
+            this.labelTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTempo.Location = new System.Drawing.Point(15, 25);
+            this.labelTempo.Name = "labelTempo";
+            this.labelTempo.Size = new System.Drawing.Size(158, 61);
+            this.labelTempo.TabIndex = 0;
+            this.labelTempo.Text = "00:00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(381, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(55, 13);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(93, 16);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.Text = "Descansando";
+            // 
+            // timerEstudo
+            // 
+            this.timerEstudo.Interval = 1000;
+            this.timerEstudo.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(292, 170);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "SempreNoTopo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelProxDescanso);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.labelCiclos);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.labelStatus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações";
+            // 
+            // labelProxDescanso
+            // 
+            this.labelProxDescanso.AutoSize = true;
+            this.labelProxDescanso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProxDescanso.Location = new System.Drawing.Point(102, 61);
+            this.labelProxDescanso.Name = "labelProxDescanso";
+            this.labelProxDescanso.Size = new System.Drawing.Size(64, 16);
+            this.labelProxDescanso.TabIndex = 7;
+            this.labelProxDescanso.Text = "5 minutos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Próximo descanso:";
+            // 
+            // labelCiclos
+            // 
+            this.labelCiclos.AutoSize = true;
+            this.labelCiclos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCiclos.Location = new System.Drawing.Point(55, 39);
+            this.labelCiclos.Name = "labelCiclos";
+            this.labelCiclos.Size = new System.Drawing.Size(26, 16);
+            this.labelCiclos.TabIndex = 5;
+            this.labelCiclos.Text = "0/4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ciclos:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Estado:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelTempo);
+            this.groupBox2.Location = new System.Drawing.Point(214, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(179, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tempo";
+            // 
+            // timerDescanso
+            // 
+            this.timerDescanso.Interval = 1000;
+            this.timerDescanso.Tick += new System.EventHandler(this.timerDescanso_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 190);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pomodoro";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTempo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Timer timerEstudo;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Timer timerDescanso;
+        private System.Windows.Forms.Label labelProxDescanso;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelCiclos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+
+        public int segundos = 00;
+        public int minutos = 00;
+        public string segundos2 = "";
+        public string minutos2 = "";
+        public bool estudando = false;
+        public int ciclos = 0;
+        public int tempoDeDescanso = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            segundos++;
+            if (segundos == 60)
+            {
+                minutos++;
+                segundos = 0;
+            }
+            if (segundos <= 9)
+            {
+                segundos2 = "0" + Convert.ToString(segundos);
+            }
+            else
+            {
+                segundos2 = segundos.ToString();
+            }
+            if(minutos <= 9)
+            {
+                minutos2 = "0" + minutos.ToString();
+            }
+            else
+            {
+                minutos2 = minutos.ToString();
+            }
+            labelTempo.Text = string.Format("{0}:{1}", minutos2, segundos2);
+
+            if(minutos == 25)
+            {
+                labelTempo.Text = "00:00";
+                minutos = 0;
+                segundos = 0;
+                timerDescanso.Enabled = true;
+                timerEstudo.Enabled = false;               
+                labelStatus.Text = "Descansando";
+                ciclos++;
+                labelCiclos.Text = String.Format("{0}/4", ciclos);
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\Delgado\source\repos\Pomodoro\Pomodoro\bin\Debug\pausaPapiro.wav");
+                simpleSound.Play();
+            }
+
+            if (ciclos < 4)
+            {
+                tempoDeDescanso = 5;        
+            }
+
+            if (ciclos == 4)
+            {
+                tempoDeDescanso = 25;
+                ciclos = 0;
+            }
+            if(ciclos == 3)
+            {
+                labelProxDescanso.Text = "25 minutos";
+            }
+            else
+            {
+                labelProxDescanso.Text = "5 minutos";
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(estudando == false)
+            {
+                labelStatus.Text = "Estudando";
+                estudando = true;
+                timerEstudo.Enabled = true;
+                button1.Text = "Recomeçar";
+            }
+            else
+            {
+                button1.Text = "Iniciar";
+                estudando = false;
+                timerEstudo.Enabled = false;
+                timerDescanso.Enabled = false;
+                labelStatus.Text = "Descansando";
+                labelCiclos.Text = "0/4";
+                labelProxDescanso.Text = "5 minutos";
+                segundos = 0;
+                minutos = 0;
+                segundos2 = "";
+                minutos2 = "";
+                ciclos = 0;
+                tempoDeDescanso = 5;
+                labelTempo.Text = "00:00";
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                base.TopMost = true;
+            }
+            else
+            {
+                base.TopMost = false;
+            }
+        }
+
+        private void timerDescanso_Tick(object sender, EventArgs e)
+        {
+            segundos++;
+            if (segundos == 60)
+            {
+                minutos++;
+                segundos = 0;
+            }
+            if (segundos <= 9)
+            {
+                segundos2 = "0" + Convert.ToString(segundos);
+            }
+            else
+            {
+                segundos2 = segundos.ToString();
+            }
+            if (minutos <= 9)
+            {
+                minutos2 = "0" + minutos.ToString();
+            }
+            else
+            {
+                minutos2 = minutos.ToString();
+            }
+            labelTempo.Text = string.Format("{0}:{1}", minutos2, segundos2);
+
+            if (minutos == tempoDeDescanso)
+            {
+                labelTempo.Text = "00:00";
+                minutos = 0;
+                segundos = 0;
+                timerDescanso.Enabled = false;
+                timerEstudo.Enabled = true;
+                labelStatus.Text = "Estudando";
+                SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\Delgado\source\repos\Pomodoro\Pomodoro\bin\Debug\voltaPapiro.wav");
+                simpleSound.Play();
+            }
+            if(ciclos == 0)
+            {
+                labelCiclos.Text = "0/4";
+            }
+        }
+    }
+}
